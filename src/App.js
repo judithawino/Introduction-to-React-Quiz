55b2c86ddccd1f65185dcd449015112e462b1752
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-07-18 10:48:17 +0300

    react children

--- src/App.js
+++ src/App.js
@@ -13,49 +13,56 @@ const users = [
   { name: "Billy Doe", id: 3 }
 ];
 
+const userList=users.map((user) =>{
+  return <li key={user.id}>{user.name}</li>
+})
+
 // comment this out after completion and uncomment code below it to proceed
 function Child() {
   return <div>This is children content</div>;
 }
-/**
-  Challenge: Uncomment this code to complete quiz
 
-function Child() {
-  return (
-    <>
-      <div>Child</div>
-      <button>Change Parent Value</button>
-    </>
-  );
-}
 
-function Parent() {
-  const [value, setValue] = React.useState(
-    "I need to be updated from my child"
-  );
 
-  return (
-    <>
-      <h3>Update Parent State Challenge (Using Callback)</h3>
-      <div className="wrapper">
-        <div>Parent</div>
-        <div className="box-wrapper">{value}</div>
-      </div>
+  // Challenge: Uncomment this code to complete quiz
 
-      <div className="wrapper">
-        <Child />
-      </div>
-    </>
-  );
-}
-Uncomment this to tackle quiz
-**/
+// function Child() {
+//   return (
+//     <>
+//       <div>Child</div>
+//       <button>Change Parent Value</button>
+//     </>
+//   );
+// }
+
+// function Parent() {
+//   const [value, setValue] = React.useState(
+//     "I need to be updated from my child"
+//   );
+
+//   return (
+//     <>
+//       <h3>Update Parent State Challenge (Using Callback)</h3>
+//       <div className="wrapper">
+//         <div>Parent</div>
+//         <div className="box-wrapper">{value}</div>
+//       </div>
+
+//       <div className="wrapper">
+//         <Child />
+//       </div>
+//     </>
+//   );
+// }
+// Uncomment this to tackle quiz
+
 
 // Comment out after completion
 function Parent() {
   return (
     <div>
       <h3>Parent Component</h3>
+      <Child/>
     </div>
   );
 }
@@ -63,20 +70,27 @@ function Parent() {
 
 function App() {
   const [] = React.useState(true);
+
+  function toggleChange(){
+    
+
+  }
   return (
     
     <>
     
     <img src={reactjsx} className="App-logo" alt="logo" />
-      <h3>User names</h3>
-      <p>JSX is cool!</p>
-      <ul></ul>
-      <button>Hide Element Below</button>
+     <p>JSX is cool!</p>
+      <h3>User names</h3>      
+      <ul>
+        {userList}
+      </ul>
+      <button onClick={toggleChange}>Hide Element Below</button>
 
       <div>Toggle Challenge</div>
       <Parent>
       <Child />
-    </Parent>
+      </Parent>
     </>
   );
 }
